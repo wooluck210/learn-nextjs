bd6e22ea6b27e46e134a9785684e5053d679326e
wooluck210 <54842493+wooluck210@users.noreply.github.com>
2024-03-14 09:14:07 +0900

    movie-info and movie-video

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import Movie from "../../components/movie"
+import styles from "../../styles/home.module.css"
 
 export const metadata = {
     title:"Home",
@@ -7,7 +9,6 @@ export const metadata = {
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
 
 async function getMovies() {
-    // await new Promise((resolve) => setTimeout(resolve, 1000))
     const response = await fetch(API_URL)
     const json = await response.json()
     return json
@@ -16,8 +17,10 @@ async function getMovies() {
 export default async function HomePage() {
     const movies = await getMovies()
     return (
-        <div>
-            {movies.map(movie => <li key={movie.id}><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}
+        <div className={styles.container}>
+            {movies.map((movie) => (
+                <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title} />
+            ))}
         </div>
     )
 }
--- components/movie-video.tsx
+++ components/movie-video.tsx
@@ -1,8 +1,8 @@
 import { API_URL } from "../app/(home)/page"
+import styles from "../styles/movie-video.module.css"
 
 async function getVideos(id: string) {
-    console.log(`Fetching videos: ${Date.now()}`)
-    await new Promise((resolve) => setTimeout(resolve, 3000))
+    // await new Promise((resolve) => setTimeout(resolve, 1000))
     // throw new Error('something broke...')
     const response = await fetch(`${API_URL}/${id}/videos`)
     return response.json()
@@ -10,5 +10,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
     const videos = await getVideos(id)
-    return <h6>{JSON.stringify(videos)}</h6>
+    return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
