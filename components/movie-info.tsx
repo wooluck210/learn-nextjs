7b772b04032452d818606e0ffef87a7a4bf7873a
wooluck210 <54842493+wooluck210@users.noreply.github.com>
2024-03-14 11:29:24 +0900

    API_URL 변수 파일 설정

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,5 +1,5 @@
 import styles from "../styles/movie-info.module.css";
-import { API_URL } from "../app/(home)/page"
+import { API_URL } from "../app/about-us/constants"
 
 export async function getMovie(id: string) {
     // await new Promise((resolve) => setTimeout(resolve, 1000))
